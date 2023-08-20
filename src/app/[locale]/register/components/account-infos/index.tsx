d0c920d61ef2c4080d7b286027e7c81395b0c534
Joseph Haase <joseph.haase.dev@gmail.com>
2023-08-20 13:09:31 -0300

    Creates the base of the account infos component

---
+++ src/app/[locale]/register/components/account-infos/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function AccountInfos(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}></div>
+    </div>
+  );
+}
