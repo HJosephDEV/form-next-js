426b1a091b9d1b7ef8b7c8c35bf68908cdc91c43
Joseph Haase <joseph.haase.dev@gmail.com>
2023-08-21 01:31:11 -0300

    Changes the steps component

--- src/app/[locale]/register/components/steps/index.tsx
+++ src/app/[locale]/register/components/steps/index.tsx
@@ -1,14 +1,16 @@
+import useRegisterStore from '@/stores/register-store';
 import styles from './styles.module.scss';
 
 export default function Steps(): JSX.Element {
+  const { showLanguageForm, showAccountForm, showAccountInfos } = useRegisterStore();
   return (
     <section className={styles.wrapperSteps}>
       <div className={styles.stepsContainer}>
-        <div className={`${styles.circle} ${styles.active}`} />
+        <div className={`${styles.circle} ${showLanguageForm ? styles.active : ''}`} />
         <div className={styles.line} />
-        <div className={styles.circle} />
+        <div className={`${styles.circle} ${showAccountForm ? styles.active : ''}`} />
         <div className={styles.line} />
-        <div className={styles.circle} />
+        <div className={`${styles.circle} ${showAccountInfos ? styles.active : ''}`} />
       </div>
     </section>
   );
