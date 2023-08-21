c343199d61882da433e7c2c695b2fece3fc3b3a2
Joseph Haase <joseph.lima@codenapp.com>
2023-08-21 12:38:13 -0300

    Fixes implementation of the zustand

--- src/stores/register-store/index.tsx
+++ src/stores/register-store/index.tsx
@@ -11,15 +11,15 @@ const states: States = {
 };
 
 const actions: StateCreator<States & Actions, [], [], Actions> = (set) => ({
-  updateBackToLanguageForm: (state) => set({ backToLanguageForm: state }),
-  updateLanguageButtons: (state) => set({ languageButtons: state }),
+  updateBackToLanguageForm: (state) => set(() => ({ backToLanguageForm: state })),
+  updateLanguageButtons: (state) => set(() => ({ languageButtons: state })),
   revealLanguageForm: () =>
-    set({ showLanguageForm: true, showAccountForm: false, showAccountInfos: false }),
+    set(() => ({ showLanguageForm: true, showAccountForm: false, showAccountInfos: false })),
   revealAccountForm: () =>
-    set({ showLanguageForm: false, showAccountForm: true, showAccountInfos: false }),
+    set(() => ({ showLanguageForm: false, showAccountForm: true, showAccountInfos: false })),
   revealAccountInfos: () =>
-    set({ showLanguageForm: false, showAccountForm: false, showAccountInfos: true }),
-  updateFields: (state) => set({ formFields: state })
+    set(() => ({ showLanguageForm: false, showAccountForm: false, showAccountInfos: true })),
+  updateFields: (state) => set(() => ({ formFields: state }))
 });
 
 export const useRegisterStore = create<States & Actions>((...args) => ({
